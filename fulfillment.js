540ea8ea7fbf27fe36e06b523bc09371ba8d5261
Giji <gijinj@hotmail.com>
2020-06-22 17:49:06 +0530

    added programs + degree

--- fulfillment.js
+++ fulfillment.js
@@ -7,20 +7,39 @@ function getResponse(request) {
     var intent = request.queryResult.intent.name;
     var params = request.queryResult.parameters;
 
+    let filteredObjects;
+    let formatType = "course";
     let responseMessage;
 
     switch (intent) {
         case 'projects/acl-xhatmt/agent/intents/03272597-3cf6-41a6-ae5b-d4ce96847496': //Get specific degree courses
-            responseMessage = getDegreeCourses(params.degreetype.toLowerCase());
+            filteredObjects = getDegreeCourses(params.degreetype.toLowerCase());
             break;
 
         case 'projects/acl-xhatmt/agent/intents/d411bdfa-61fa-4902-ab37-615f076ad200': //Get specific program courses
-            responseMessage = getCoursesByProgram(params.programtype.toLowerCase());
+            filteredObjects = getCoursesByProgram(params.programtype.toLowerCase());
             break;
 
         case 'projects/acl-xhatmt/agent/intents/febeef3b-0a44-446c-a171-70b5de8bb306': //Get courses
-            responseMessage = getCourses(params.degreetype.toLowerCase(), params.programtype.toLowerCase());
+            filteredObjects = getCourses(params.degreetype.toLowerCase(), params.programtype.toLowerCase());
             break;
+
+        case 'projects/acl-xhatmt/agent/intents/4e610f80-0529-4258-835f-a2e7cae8d8d5': //Get all programs
+            filteredObjects = getPrograms();
+            formatType = "program";
+            break;
+
+        case 'projects/acl-xhatmt/agent/intents/3bd6fcd4-df61-4265-bda3-65109a0dc0c6': //Get Degrees
+            filteredObjects = getDegrees();
+            formatType = "program";
+            break;
+    }
+
+    if (formatType == "course") {
+        responseMessage = formatCourses(filteredObjects);
+    }
+    else {
+        responseMessage = formatPrograms(filteredObjects);
     }
 
     return createResponse(responseMessage);
@@ -43,20 +62,30 @@ function formatCourses(courses) {
     })*/
 
     var message = courses.map((course) => {
-        return '<p>' + course.name + '<br/>More details are <a href="'+ course.link+'" target="_blank">here</a>'+'</p>';
+        return '<p>' + course.name + '<br/>More details are <a href="' + course.link + '" target="_blank">here</a>' + '</p>';
     }).join('');
 
-    return '<p>'+ courses.length +' courses found: </p>' + message;
+    return '<p>' + courses.length + ' courses found: </p>' + message;
 }
 
 function formatCourse(course) {
+    var message = '<p>' + 'The course details:' + '</p>' +
+        '<p><b>' + course.name + '</b><br/>' + course.description + '</p>' +
+        'More details are <a href="' + course.link + '" target="_blank">here</a>' + '</p>';
 
-    
 
-    var message = '<p>'+'The course details:'+'</p>'+
-                    '<p><b>'+ course.name + '</b><br/>'+ course.description + '</p>'+
-                        'More details are <a href="'+ course.link+'" target="_blank">here</a>'+'</p>';
+    return message;
+}
 
+function formatPrograms(values) {
+
+    if (values.length == 0) {
+        return 'No items found.'
+    }
+
+    var message = values.map((value) => {
+        return '<p>' + value + '</p>';
+    }).join('');
 
     return message;
 }
@@ -88,6 +117,30 @@ function getCourses(degree, programType) {
 
 }
 
+function getPrograms() {
+    var programs = coursesData.map((item) => {
+        return item.programType
+    });
+
+    return distinctOf(programs);
+}
+
+function getDegrees() {
+    var degrees = coursesData.map((item) => {
+        return item.degree
+    });
+
+    return distinctOf(degrees);
+}
+
+function distinctOf(values) {
+    return values.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+    });
+}
+
+
+
 
 function createResponse(responseMessage) {
     return {
@@ -95,7 +148,7 @@ function createResponse(responseMessage) {
             {
                 text: {
                     text: [
-                        formatCourses(responseMessage)
+                        responseMessage
                     ]
                 }
             }
